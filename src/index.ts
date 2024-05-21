fc9f792944647acb57c81afa0f0e32f53aef6a28
Rudolph Jose Razul <rudolphjose.razul@timesoftsg.com.sg>
2024-05-21 18:07:40 +0800

    updated code

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { isPalindrome } from "./palindrome";
+
+console.log("\Palindrome Checker:");
+console.log(isPalindrome("Kayak")); // true
+console.log(isPalindrome("Candidate")); // false
+console.log(isPalindrome("Radar")); // true
---
+++ src/palindrome.ts
@@ -0,0 +1,11 @@
+/*
+A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+Palindrome Checker:
+Write a function isPalindrome(input: string): boolean that returns true if the given string is a palindrome, and false otherwise.
+*/
+
+export function isPalindrome(input: string): boolean {
+  //your code here
+  return false;
+}
